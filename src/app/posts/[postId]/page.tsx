d163e0a4b683caab54eaec18f2b31e168bd9a255
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-08-31 17:57:20 +0900

    Add a plugin of tailwind typography

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,3 +1,5 @@
+import { IPost } from "@/qiita.schema.types";
+import { cl } from "@/util";
 import React from "react";
 
 type Props = {
@@ -6,7 +8,14 @@ type Props = {
   };
 };
 
-export default function Post({ params }: Props) {
-  return <div>
-  {params.postId}Post</div>;
+export default async function Post({ params: { postId } }: Props) {
+  const res = await fetch(`https://qiita.com/api/v2/items/${postId}`);
+  const post: IPost = await res.json();
+
+  return (
+    <article className="prose lg:prose-xl">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </article>
+  );
 }
