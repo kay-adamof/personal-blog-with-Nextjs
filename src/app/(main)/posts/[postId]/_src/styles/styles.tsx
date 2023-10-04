4c809ab2db378cbce4ffa44f2c87a2e334de239c
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-10-04 09:44:14 +0900

    Add styles

---
+++ src/app/(main)/posts/[postId]/_src/styles/styles.tsx
@@ -0,0 +1,7 @@
+const className = {
+  main: 'mx-auto flex min-h-screen max-w-7xl flex-col items-center justify-between p-24',
+  none: '',
+  article: 'prose dark:prose-invert',
+}
+
+export {className as cn}
