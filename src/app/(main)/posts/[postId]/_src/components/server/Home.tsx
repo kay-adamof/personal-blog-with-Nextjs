61cf7529782c715c5c7cad8ab7076e50856654b5
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-10-06 15:19:25 +0900

    Format URL with regex
    To identify for remark-oembed plugin

--- src/app/(main)/posts/[postId]/_src/components/server/Home.tsx
+++ src/app/(main)/posts/[postId]/_src/components/server/Home.tsx
@@ -3,17 +3,45 @@ import { rehype } from '@/lib'
 import 'highlight.js/styles/night-owl.css'
 import * as T from '@/types'
 import { CustomMDX } from './CustomMdx'
+import fs from 'fs/promises'
+import path from 'path'
+
+const filePath = () => {
+  const fileToStorePath = path.join(
+    process.cwd(),
+    'src/app/(main)/posts/[postId]/_src/components/server/article.md',
+  )
+  return fileToStorePath
+}
 
 export const Home = async ({ post }: { post: T.Post }) => {
+  const markdownFilePath = filePath()
+  const markdownContent = await fs.readFile(markdownFilePath, 'utf8')
+
+  const regex = {
+    Url: /\n(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*))/g,
+    Xcom: /https:\/\/x\.com/g,
+    Twitter:
+      /(\[https:\/\/twitter\.com\/)([A-Za-z0-9_]+)(\/status\/[0-9]+\?s=[0-9]+\])/g,
+  }
+
+  const contentAfterReplaced = markdownContent
+    .replace(regex.Url, '\n[$1]($1)')
+    .replace(regex.Xcom, 'https://twitter.com')
+    .replace(
+      regex.Twitter,
+      (_, $1, $2, $3) => $1 + $2.replace(/_/g, '\\_') + $3,
+    )
+
   return (
     <article className='prose dark:prose-invert'>
       <h1>{post.title}</h1>
       <CustomMDX
-        source={post.body}
+        source={contentAfterReplaced}
         options={{
           mdxOptions: {
             remarkPlugins: [
-              [remark.gfm],
+              // [remark.gfm],
               [
                 remark.embedder,
                 {
