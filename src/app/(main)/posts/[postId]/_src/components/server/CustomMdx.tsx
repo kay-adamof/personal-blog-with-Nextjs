8d654875ced6071a985f232394da7bdab36db5a7
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-10-07 12:07:32 +0900

    Show og image

--- src/app/(main)/posts/[postId]/_src/components/server/CustomMdx.tsx
+++ src/app/(main)/posts/[postId]/_src/components/server/CustomMdx.tsx
@@ -1,19 +1,76 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
-import React from 'react'
+import React, { AnchorHTMLAttributes, ReactNode } from 'react'
 import { Code } from './Code'
+import * as lib from '../../lib'
 
-const H2component = (props: React.PropsWithChildren): JSX.Element => (
-  <h2
-    {...props}
-    className='text-red-300'
-  >
-    {props.children}
-  </h2>
-)
+// ---------------
+import urlMetadata from 'url-metadata'
+import Image from 'next/image'
+
+
+const Anchor = async (
+  props: AnchorHTMLAttributes<HTMLAnchorElement>,
+): Promise<JSX.Element> => {
+  const href = props.href
+
+  let metaImage: urlMetadata.Result[string] = ''
+
+  if (href) {
+    if (lib.isUrl(href)) {
+      const url = new URL(href)
+      const oembedProviderUrls = await lib.getOembedProviderUrls()
+      const hasUrlOfOembedProvider = (urls: string[]) => {
+        return urls
+          .map(providerUrl => providerUrl.includes(url.hostname))
+          .some(v => v === true)
+      }
+      if (!hasUrlOfOembedProvider(oembedProviderUrls)) {
+        try {
+          const metaData = await urlMetadata(url.toString())
+
+          if (typeof metaData['og:image'] === 'string') {
+            metaImage = metaData['og:image']
+          }
+        } catch (error) { }
+      }
+    }
+  }
+
+  const Card: ReactNode = (
+    <>
+      <div className=''>
+        <span>Test</span>
+
+        <Image
+          src={metaImage}
+          width={500}
+          height={500}
+          alt='Picture of the author'
+        />
+      </div>
+    </>
+  )
+  return (
+    <>
+      <a {...props}>
+        {metaImage ? (
+          <Image
+            src={metaImage}
+            width={500}
+            height={500}
+            alt='Picture of the author'
+          />
+        ) : (
+          props.children
+        )}
+      </a>
+    </>
+  )
+}
 
 const components = {
-  h2: H2component,
   code: Code,
+  a: Anchor,
 }
 
 export function CustomMDX(props: any) {
