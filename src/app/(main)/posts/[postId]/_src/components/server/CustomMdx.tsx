50251790061b76b018b2214d4ea6c08e72621f3d
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-10-11 09:00:36 +0900

    Separeta visibility when changing display width

--- src/app/(main)/posts/[postId]/_src/components/server/CustomMdx.tsx
+++ src/app/(main)/posts/[postId]/_src/components/server/CustomMdx.tsx
@@ -1,77 +1,68 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
-import React, { AnchorHTMLAttributes, ReactNode } from 'react'
+import React, { AnchorHTMLAttributes, HTMLAttributes } from 'react'
 import { Code } from './Code'
 import * as lib from '../../lib'
-
-// ---------------
 import urlMetadata from 'url-metadata'
 import Image from 'next/image'
+import { keyofMetadata } from '../../constants'
 
+const getMeta =
+  (metaData: Awaited<ReturnType<typeof urlMetadata>>) =>
+    (str: keyofMetadata) => {
+      const data = metaData[str]
+      return typeof data === 'string' ? data : ''
+    }
 
 const Anchor = async (
   props: AnchorHTMLAttributes<HTMLAnchorElement>,
-): Promise<JSX.Element> => {
-  const href = props.href
+): Promise<JSX.Element | string> => {
+  const defaultAnchor: JSX.Element = <a {...props} />
 
-  let metaImage: urlMetadata.Result[string] = ''
+  if (!props.href) return defaultAnchor
 
-  if (href) {
-    if (lib.isUrl(href)) {
-      const url = new URL(href)
-      const oembedProviderUrls = await lib.getOembedProviderUrls()
-      const hasUrlOfOembedProvider = (urls: string[]) => {
-        return urls
-          .map(providerUrl => providerUrl.includes(url.hostname))
-          .some(v => v === true)
-      }
-      if (!hasUrlOfOembedProvider(oembedProviderUrls)) {
-        try {
-          const metaData = await urlMetadata(url.toString())
+  if (!lib.isUrl(props.href)) return defaultAnchor
 
-          if (typeof metaData['og:image'] === 'string') {
-            metaImage = metaData['og:image']
-          }
-        } catch (error) { }
-      }
-    }
+  const url = new URL(props.href)
+  const oembedProviderUrls = await lib.getOembedProviderUrls()
+  const hasUrlOfOembedProvider = (urls: string[]) => {
+    return urls
+      .map(providerUrl => providerUrl.includes(url.hostname))
+      .some(v => v === true)
   }
 
-  const Card: ReactNode = (
-    <>
-      <div className=''>
-        <span>Test</span>
+  if (hasUrlOfOembedProvider(oembedProviderUrls)) return defaultAnchor
 
-        <Image
-          src={metaImage}
-          width={500}
-          height={500}
-          alt='Picture of the author'
-        />
-      </div>
-    </>
-  )
-  return (
-    <>
-      <a {...props}>
-        {metaImage ? (
-          <Image
-            src={metaImage}
-            width={500}
-            height={500}
-            alt='Picture of the author'
-          />
-        ) : (
-          props.children
-        )}
-      </a>
-    </>
-  )
+  try {
+    const metaData = await urlMetadata(url.toString())
+    const meta = getMeta(metaData)
+
+    return (
+      <>
+        <div className='card md:card-side'>
+          <div className='card-body'>
+            <h2 className='card-title'>{meta('og:title')}</h2>
+            <p>{meta('og:description')}</p>
+          </div>
+          <figure className='invisible md:visible'>
+            <Image
+              src={meta('og:image')}
+              width={1200}
+              height={630}
+              alt=''
+            />
+          </figure>
+        </div>
+      </>
+    )
+  } catch (error) {
+    return defaultAnchor
+  }
 }
 
 const components = {
   code: Code,
   a: Anchor,
-  p: (props: HTMLAttributes<HTMLDivElement>) => (
+  p: (props: HTMLAttributes<'div'>) => (
     <div className='my-5'>{props.children}</div>
   ),
 }
