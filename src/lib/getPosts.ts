41b6d82454afc1212430fdade4c81561785d38d0
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-10-13 14:45:55 +0900

    Change name to import types

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,8 +1,9 @@
 import * as T from '@/types'
 import * as CT from '@/constants'
+import type { Qiita } from '@/types'
 
 export default async () => {
   const res = await fetch(CT.itemsUrl,{ next: { revalidate: 3600 } })
-  const posts: T.Post[] = await res.json()
+  const posts: Qiita.Post[] = await res.json()
   return posts
 }
