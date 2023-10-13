cf6817083a17663bb0f22c6ef537251f6a2370b0
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-10-13 14:41:26 +0900

    Delete background color

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,7 +2,7 @@ import Posts from "@/components/Posts"
 
 export default ()=> {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between bg-gray-50 p-24 dark:bg-gray-800'>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Posts />{' '}
     </main>
   )
