f0b37a3672164dc5f05b37988a6561222442405f
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-10-23 12:46:52 +0900

    Update index

--- src/types/Index.ts
+++ src/types/Index.ts
@@ -2,6 +2,7 @@ export * from './rehype-react'
 export * as Qiita from './qiita.schema.types'
 export * from './twitterResponse'
 export * from './props'
+export * from './LocalStorageTheme'
 export type MyTheme = 'lemonade' | 'night'
 
 
