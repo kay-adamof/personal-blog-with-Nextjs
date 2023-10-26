2d05dbc2f64149ef64ddf10272bc5e3254b11815
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-10-26 13:55:44 +0900

    Move navbar to root

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,15 @@
-import Navbar from "@/components/Navbar"
+import * as S from '@server_components'
+import * as C from '@client_components'
 import Posts from "@/components/Posts"
 
 export default ()=> {
   return (
     <> 
-      <Navbar />
+      <S.Navbar 
+        left={<S.LinkToRoot />}
+        ToggleThemeButton={<C.ToggleThemeButton />}
+        ToggleTocButton={<S.ToggleTocButton />}
+      />
       <main className='flex min-h-screen flex-col items-center justify-between p-24'>
         <Posts />{' '}
       </main>
