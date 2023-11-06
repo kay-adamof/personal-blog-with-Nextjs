d0ab4c20cf1885eac2c1a707e33f62254b3ea185
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-11-06 10:42:10 +0900

    Stop using handler

--- src/components/TwitterBackgroundColor.tsx
+++ src/components/TwitterBackgroundColor.tsx
@@ -1,22 +1,20 @@
 import Script from 'next/script'
 
-const handleLoad = () => {
-  twttr.ready(twttr => {
-    twttr.events.bind('rendered', (event: Event) => {
-      // console.log(event.target.querySelector('iframe'))
-      const iframe = event.target.querySelector('iframe') as HTMLIFrameElement
-      const iframeGlobal= document.getElementById(`${iframe.id}`) as HTMLIFrameElement
-      console.log(iframeGlobal.querySelector('div'))
-    })
-  })
-}
+// const handleLoad = () => {
+//   twttr.ready(twttr => {
+//     twttr.events.bind('rendered', (event: Event) => {
+//       const iframe = event.target.querySelector('iframe') as HTMLIFrameElement
+//       const iframeGlobal= document.getElementById(`${iframe.id}`) as HTMLIFrameElement
+//       console.log(iframeGlobal.querySelector('div'))
+//     })
+//   })
+// }
 
 export const TwitterBackgroundColor = () => {
   return (
     <>
       <Script
         src='https://platform.twitter.com/widgets.js'
-        onLoad={handleLoad}
       />
     </>
   )
