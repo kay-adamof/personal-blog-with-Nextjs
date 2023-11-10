d49acc1f22bd330c33e7e16f798cdb86d6d8ced2
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-11-10 19:38:11 +0900

    SSG

--- src/app/(main)/posts/[postId]/page.tsx
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -2,14 +2,21 @@ import * as L from '@/lib'
 import * as S from '@server_components'
 import * as C from '@client_components'
 
+export async function generateStaticParams() {
+  const posts = await L.getPosts()
+
+  return posts.map(post => ({
+    slug: post.id,
+  }))
+}
+
 export default async function PostPage({
   params: { postId },
 }: {
   params: {
     postId: string
   }
-}){
-  
+}) {
   const post = await L.getPost(postId)
 
   return (
