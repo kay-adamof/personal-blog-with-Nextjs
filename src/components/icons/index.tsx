9105fd6e8b2b687c79675b8ca06e166420ddbdf1
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-11-10 12:45:05 +0900

    Fix display-name issue

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,25 +1,39 @@
-import {
-  IconMoonStars,
-  IconSunHigh,
-  IconListTree,
-  TablerIconsProps,
-  IconSun,
-} from '@tabler/icons-react'
+import * as TI from '@tabler/icons-react'
 
 const size = '1.8rem' as const
 export type DefaultIconSize = typeof size
 
-const MakeIcon = (IconName: (props: TablerIconsProps) => JSX.Element) => {
-  return (props: Omit<TablerIconsProps, 'size'>): JSX.Element => (
-    <IconName
+// const MakeIcon = (IconName: (props: TablerIconsProps) => JSX.Element) => {
+//   return (props: Omit<TablerIconsProps, 'size'>): JSX.Element => (
+//     <IconName
+//       size={size}
+//       {...props}
+//     />
+//   )
+// }
+
+export const LightModeIcon = (props: TI.TablerIconsProps) => {
+  return (
+    <TI.IconSunHigh
+      size={size}
+      {...props}
+    />
+  )
+}
+export const DarkModeIcon = (props: TI.TablerIconsProps) => {
+  return (
+    <TI.IconMoonStars
+      size={size}
+      {...props}
+    />
+  )
+}
+export const TocIcon = (props: TI.TablerIconsProps) => {
+  return (
+    <TI.IconListTree
       size={size}
       {...props}
     />
   )
 }
 
-export const LightModeIcon = MakeIcon(IconSun)
-
-export const DarkModeIcon = MakeIcon(IconMoonStars)
-
-export const TocIcon = MakeIcon(IconListTree)
