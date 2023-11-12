18e3317b1f1dd97ce4efba298f4f240241e544ae
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-11-12 19:48:09 +0900

    Add style and change the title

--- src/components/GoToRoot.tsx
+++ src/components/GoToRoot.tsx
@@ -2,6 +2,6 @@ import Link from 'next/link'
 
 export const GoToRoot = () => (
   <Link href={'/'}>
-    <h1 className=''>My Gorgeous Blog</h1>
+    <h1 className='text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-purple-500'>Blog</h1>
   </Link>
 )
